b60c2fd89c5f2a1b1a6b8c9d5a8eda9cff13a38c
Blask <blaskyy@gmail.com>
2012-12-14 12:30:35 +0800

    change line

--- lab11/iptest.sh
+++ lab11/iptest.sh
@@ -4,14 +4,15 @@ iptables -t filter -F
 iptables -t nat -F
 iptables -t mangle -F
 iptables -t raw -F
-#2
-iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-#3
-iptables -A OUTPUT -p icmp -j DROP
 #4
 iptables -P INPUT DROP
 #5
 iptables -P OUTPUT ACCEPT
 #6
 iptables -P FORWARD DROP
+#2
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+#3
+iptables -A OUTPUT -p icmp -j DROP
+
 
