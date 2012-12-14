138fffb44f632cb0b90505c39c8d8a4a0152f09b
Blask <blaskyy@gmail.com>
2012-12-15 01:09:29 +0800

    fix iptest2.sh

--- lab11/iptest2.sh
+++ lab11/iptest2.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
 #2
-iptables -I INPUT -p icmp -j ACCEPT
+iptables -I OUTPUT -j DROP
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -I OUTPUT -p icmp -j ACCEPT
 #3
 iptables -I INPUT -p tcp --dport 22 -j DROP
 
