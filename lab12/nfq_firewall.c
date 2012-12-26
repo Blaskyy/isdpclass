1e55c3f3ff3dc2803d3275ca8b1b70db70d148b7
Blask <blaskyy@gmail.com>
2012-12-26 23:58:15 +0800

    add comment && fix the bugs

--- lab12/nfq_firewall.c
+++ lab12/nfq_firewall.c
@@ -9,19 +9,30 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 
 #define TO "114.114.114.114"
+#define LENGTH 4096
+
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *nfa, void *data){
-    int id = 0;
+    int id = 0, pdata_len;
+    unsigned char *pdata;
     struct nfqnl_msg_packet_hdr *ph;
+
+    //get unique ID of packet in queue
     ph = nfq_get_msg_packet_hdr(nfa);
     if(ph) {
         id = ntohl(ph->packet_id);
     }
-    unsigned char *pdata;
-    int pdata_len;
+
+    //get payload
     pdata_len = nfq_get_payload(nfa, &pdata);
     if(pdata_len == -1) {
         pdata_len = 0;
     }
+
+    /* get the dest addr of this packet;
+     * compare the dest addr with TO
+     * if they are the same,then return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL)
+     * else return nfq_set_verdict(qh, id, NF_DROP, 0, NULL)
+     */
     struct iphdr *iphdrp = (struct iphdr *)pdata;
     if(iphdrp->daddr == inet_addr(TO)){
         printf("Access ping to ");
@@ -35,43 +46,49 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg, struct nfq_data *
 
 int main(int argc, const char *argv[])
 {
+    int len, fd;
+    char buf[LENGTH];
     struct nfq_handle *h;
     struct nfq_q_handle *qh;
-    int fd;
-    int rv;
-    char buf[4096] __attribute__ ((aligned));
+
+    //call nfq_open() to open a NFQUEUE handler
     h = nfq_open();
     if(!h) {
         fprintf(stderr, "error during nfq_open()\n");
         exit(1);
     }
 
-    printf("unbinging existing nf_queue handler for PE_INET(if any)\n");
+    //unbinging existing nf_queue handler for PE_INET(if any)
     if(nfq_unbind_pf(h, PF_INET) < 0) {
         fprintf(stderr, "error during nfq_unbind_pf()\n");
         exit(1);
     }
 
-    printf("binding nfnetlink_queue as nf_queue handler for PF_INET\n");
+    //binding nfnetlink_queue as nf_queue handler for PF_INET
     if(nfq_bind_pf(h, PF_INET) < 0) {
         fprintf(stderr, "error during nfq_bind_pf()\n");
         exit(1);
     }
 
-    printf("binding this socket to queue '0'\n");
+    //binding this socket to queue 
     qh = nfq_create_queue(h, 0, &cb, NULL);
     if(!qh) {
         fprintf(stderr,"error during nfq_create_queue()\n");
         exit(1);
     }
 
-    printf("setting copy_packet mode\n");
+    //setting copy_packet mode
     if(nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
         fprintf(stderr, "can't set packet_copy_mode\n");
         exit(1);
     }
-    while (1) {
-        nfq_handle_packet(h, buf, rv);
+
+    //get the file descriptor associated with the nfqueue handler
+    fd = nfq_fd(h);
+
+    //handle a packet received from the nfqueue subsystem
+    while ((len = recv(fd, buf, sizeof(buf), 0)) && len >= 0) {
+        nfq_handle_packet(h, buf, len);
     }
 
     nfq_destroy_queue(qh);
